83ef74a4de4d5653e6f8db29212c5235b6755534
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-07-02 20:34:04 -0300

    Criada classe Conta

--- Conta.java
+++ Conta.java
@@ -1,2 +1,24 @@
-public class Conta {
+public abstract class Conta {
+    protected String titular;
+    protected Double saldo;
+
+    public Conta(String titular, Double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public abstract void atualizarSaldo();
+
+    public void depositar(double valor) {
+        System.out.println(saldo + valor);
+    }
+
+    public void sacar(double valor) {
+        System.out.println(saldo - valor);
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome do titular: " + titular);
+        System.out.println("Saldo: " + saldo);
+    }
 }
